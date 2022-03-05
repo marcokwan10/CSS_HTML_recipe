80953f084bc82fbc86a7f139e7839d9629aabbb4
Marco Kwan <marcokwan10@gmail.com>
2022-03-05 18:09:25 -0500

    navbar toggle

--- js/app.js
+++ js/app.js
@@ -1,19 +1,12 @@
 const getElement = (selector) => {
-  const element = document.querySelector(selector)
+	const element = document.querySelector(selector);
+	if (element) return element;
+	throw Error(`No "${selector}" class, check your code`);
+};
 
-  if (element) return element
-  throw Error(
-    `Please double check your class names, there is no ${selector} class`
-  )
-}
+const links = getElement(".nav-links");
+const navBtn = getElement(".nav-btn");
 
-const links = getElement('.nav-links')
-const navBtnDOM = getElement('.nav-btn')
-
-navBtnDOM.addEventListener('click', () => {
-  links.classList.toggle('show-links')
-})
-
-const date = getElement('#date')
-const currentYear = new Date().getFullYear()
-date.textContent = currentYear
+navBtn.addEventListener("click", () => {
+	links.classList.toggle("show-links");
+});
